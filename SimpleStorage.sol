4d341e5870c1c20f5f0484a07312d766f47ca4d6
Kenar-MC <kenar.fresh@gmail.com>
2024-12-23 00:23:16 +0300

    asdasd

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -5,6 +5,17 @@ pragma solidity ^0.8.18; // Version
 
 contract SimpleStorage 
 {
+
     bool hasFavoriteNumber = true;  
+    uint256 favoriteNumber = 88; // Set to 0 if not assigned
+    int256 favoriteNegativeNumber = -88; 
+    string favoriteString = "88"; 
+    address favoriteAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; 
+
+    function store(uint256 _favoriteNewNumber) public
+    {
+        favoriteNumber = _favoriteNewNumber;
+    }
+
 }
 
