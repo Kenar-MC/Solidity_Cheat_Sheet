65d58750a46d5688d29c46e9aa891a5b6d2bb43b
Kenar-MC <kenar.fresh@gmail.com>
2024-12-23 00:42:57 +0300

    Make favoriteNumber public and add visibility comments in SimpleStorage contract

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -7,11 +7,13 @@ contract SimpleStorage
 {
 
     bool hasFavoriteNumber = true;  
-    uint256 favoriteNumber; // Set to 0 if not assigned
+    uint256  public favoriteNumber; // Set to 0 if not assigned
     int256 favoriteNegativeNumber = -88; 
     string favoriteString = "88"; 
     address favoriteAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; 
 
+        // Visibility : public (can be accessed from outside the contract), private (can only be accessed from within the contract)
+        // internal (can only be accessed from within the contract and contracts deriving from it), external (can only be accessed from outside the contract)
     function store(uint256 _favoriteNewNumber) public
     {
         favoriteNumber = _favoriteNewNumber;
