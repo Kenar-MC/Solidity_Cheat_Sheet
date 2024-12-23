76309f6b73250410a5ba4ac6d4f0f539dff4488e
Kenar-MC <kenar.fresh@gmail.com>
2024-12-23 23:20:27 +0300

    debug

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -6,24 +6,24 @@ pragma solidity ^0.8.18; // Version
 contract SimpleStorage 
 {
 
-    bool hasFavoriteNumber = true;  
-    uint256  public favoriteNumber; // Set to 0 if not assigned
-    int256 favoriteNegativeNumber = -88; 
-    string favoriteString = "88"; 
-    address favoriteAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; 
+    bool hasFavoriteNumber = true;                  // Boolean
+    uint256  public favoriteNumber;                 // Set to 0 if not assigned
+    int256 favoriteNegativeNumber = -88;            // 256 bit signed integer
+    string favoriteString = "88";               
+    address favoriteAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;   // Ethereum address
 
-    uint256[] favoriteArray = [1,2,3,4,5]; 
+    uint256[] favoriteArray = [1,2,3,4,5];          
 
 
-    struct Person    
+    struct Person                                   // You cant change the value of a struct after it is set
     {
         uint256 favoriteNumber;
         string name;    
     }
 
-    Person  public person = Person (88, "John");
-    Person  public person2 = Person ({favoriteNumber: 88, name: "John"});
-    Person[] public people = [Person(88, "John"), Person(88, "John")];
+    Person  public person = Person (88, "John");        // Person struct    
+    Person  public person2 = Person ({favoriteNumber: 88, name: "John"});       // Person struct
+    Person[] public people = [Person(88, "John"), Person(88, "John")];          // Array of Person struct
     
     Person[] public listOfPeople;                                       // Dynamic array
     Person[2] public listOfPeople2;                                      // Static array
