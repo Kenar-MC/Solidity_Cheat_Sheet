69c9c3fa540dea11f109bd97cba0ebeab07e94c4
Kenar-MC <kenar.fresh@gmail.com>
2024-12-23 23:16:13 +0300

    debug

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -14,28 +14,43 @@ contract SimpleStorage
 
     uint256[] favoriteArray = [1,2,3,4,5]; 
 
-    struck Person
+
+    struct Person    
     {
         uint256 favoriteNumber;
         string name;    
     }
 
+    Person  public person = Person (88, "John");
+    Person  public person2 = Person ({favoriteNumber: 88, name: "John"});
+    Person[] public people = [Person(88, "John"), Person(88, "John")];
+    
+    Person[] public listOfPeople;                                       // Dynamic array
+    Person[2] public listOfPeople2;                                      // Static array
+
+
+        function addPerson(uint256 _favoriteNumber, string memory _name) public
+        {
+            listOfPeople.push(Person(_favoriteNumber, _name)); 
+        }
+
+
         // Visibility : public, private, internal, external
 
-    function store(uint256 _favoriteNewNumber) public  /// Public means it can be accessed from outside the contract and it changes the state of the contract
-    {
-        favoriteNumber = _favoriteNewNumber;
-    }
-        
-    function retrieve () public view returns (uint256) /// View means it doesn't change the state of the contract
-    {
-        return favoriteNumber;  
-    }
+        function store(uint256 _favoriteNewNumber) public  /// Public means it can be accessed from outside the contract and it changes the state of the contract
+        {
+            favoriteNumber = _favoriteNewNumber;
+        }
+            
+        function retrieve () public view returns (uint256) /// View means it doesn't change the state of the contract
+        {
+            return favoriteNumber;  
+        }
 
-    function nothing () public pure returns (string memory) /// Pure means it doesn't read or write to the state of the contract
-    {
-        return "Hello World";
-    }
+        function nothing () public pure returns (string memory) /// Pure means it doesn't read or write to the state of the contract
+        {
+            return "Hello World";
+        }
 
 }
 
