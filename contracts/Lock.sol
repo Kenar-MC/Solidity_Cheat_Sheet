c3d7216b429bfa16b2c8167d22e6b51184b20617
Kenar-MC <kenar.fresh@gmail.com>
2025-01-05 21:57:55 +0300

    asd

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,9 +1,3 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18; // Version
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; // Importing ERC20
-
-contract BEEToken is ERC20 
-{
-
-}
