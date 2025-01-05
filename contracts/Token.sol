c93e9c3531c355991f3a0136bf73c6d0dc269b0e
Kenar-MC <kenar.fresh@gmail.com>
2025-01-05 22:39:58 +0300

    sda

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,5 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; // Importing ERC20
 
 contract BEEToken is ERC20 
 {
+    constructor() ERC20("BEE Token", "BEE") 
+    {
+        _mint(msg.sender, 1000000 * 10 ** 18); // Minting 1,000,000 tokens
+    }
+    {
 
+    }
 }
